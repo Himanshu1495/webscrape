f8aac2d4f08bdb1ab1aa55bb4ad5993cf82bb201
Himanshu Nachane <himanshu.n1495@gmail.com>
2017-06-20 01:32:43 +0530

    Major modifications. Recursion for traverse to be added

--- scrape.py
+++ scrape.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
 
 import requests
+import urllib2
 import datetime
+import re
 
 def traverse(links,already_crawled,count):
 	#check if links are present
@@ -9,27 +11,25 @@ def traverse(links,already_crawled,count):
 		for link in links:
 			#check if link is not already crawled
 			if link not in already_crawled:
-				visiting_link = link.get('href')
+				visiting_link = link
 				#if link has http:// or https://
 				if visiting_link.startswith("https://") or visiting_link.startswith("http://"):
 					print "Crawling link : %s " % visiting_link
 					#crawl link
-					count += 1
 					crawl(visiting_link,links)					
 				else:
 					#if link does not have http:// or https://, prefix it before crawling
 					visiting_link = url + visiting_link
 					print "Crawling link : %s " % visiting_link
-					count += 1
+					#crawl link
 					crawl(link,links)	
 
 def crawl(link,links):
 	try:
-		r = requests.get(link, verify=True)
-		data = r.text
-		soup = BeautifulSoup(data, "lxml")
+		r = urllib2.urlopen(link)
+		soup = BeautifulSoup(r, 'html.parser')
 		already_crawled.append(link)
-		new_links = [new_find for new_find in soup.find_all('a', href=True)]
+		new_links = [new_find for new_find in soup.findAll('a', attrs = {'href': re.compile("^"+pre)})]
 		#add new found links to previous links list 
 		links = links + new_links
 	except:
@@ -47,18 +47,17 @@ t1 = datetime.datetime.now()
 if cert=="y" or cert=="Y":
 	pre = "https://"
 	url = "https://"+ask
-	r = requests.get(url, verify=False)
+	r = urllib2.urlopen(url)
 #if SSL is not present
 else:
 	pre = "http://"
 	url = "http://"+ask
-	r = requests.get(url, verify=False)
+	r = urllib2.urlopen(url)
 
 #get the source of the page
-data  = r.text
+soup = BeautifulSoup(r,'html.parser')
 
 
-soup = BeautifulSoup(data,"lxml")
 #initialise count
 count = 0
 #initialise visited
@@ -68,7 +67,7 @@ already_crawled = []
 #get all links of the page 
 #initialise not visited
 not_crawled = []
-links = [link for link in soup.find_all('a', href=True)]
+links = [link.get('href') for link in soup.findAll('a', attrs = {'href': re.compile("^"+pre)})]
 #call traverse function
 traverse(links,already_crawled,count)
 
@@ -82,4 +81,6 @@ print "=============================================================="
 print "Total links found: %d" % len(links)
 print "Total links visited by crawlers : %d " % len(already_crawled)	
 print "Total links not visited by crawlers : %d" % len(not_crawled)
+for l in not_crawled:
+	print l
 print "Total time taken : %s" % total_time
